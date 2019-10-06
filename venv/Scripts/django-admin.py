5d519e446d0c594ed2021775e16fde3b53ae91a3
yvesvc <yvesss_710@hotmail.com>
2019-10-06 13:30:26 +0200

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\yvesvc~1\google~1\ait\xc18e~1.oth\django~2\django~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
