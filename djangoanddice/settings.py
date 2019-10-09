198506cc78d9b079e54da1093f199fe5ba09400c
yvesvc <yvesss_710@hotmail.com>
2019-10-09 19:42:33 +0200

    test

--- djangoanddice/settings.py
+++ djangoanddice/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 
 LOGIN_REDIRECT_URL = 'login'
@@ -130,7 +129,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
