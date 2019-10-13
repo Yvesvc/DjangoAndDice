fc94d7fbefb9987be4088da0250e4b17a3279e07
yvesvc <yvesss_710@hotmail.com>
2019-10-13 17:10:33 +0200

    postgresql password in .env file

--- djangoanddice/settings.py
+++ djangoanddice/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,11 +80,14 @@ WSGI_APPLICATION = 'djangoanddice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoanddice_db',
+        'USER': 'postgres',
+        'PASSWORD': env('DJANGOANDDICE_POSTGRES'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
