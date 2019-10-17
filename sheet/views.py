589e9e8a5be02f6bd129724c54ffdc876fd46f5c
yvesvc <yvesss_710@hotmail.com>
2019-10-17 21:12:10 +0200

    -test branching

--- sheet/views.py
+++ sheet/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .forms import Sheet_Form
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 @login_required
 def index(request):
+    User = get_user_model()
+    foo = User
     form = Sheet_Form()
-    return render(request, 'sheet/sheet.html', {'form': form})
+    return render(request, 'sheet/sheet.html')
