db2f193929a1ffc92af79aac937dfba31e92c9f3
yvesvc <yvesss_710@hotmail.com>
2019-10-18 20:32:25 +0200

    styling sheet app

--- sheet/views.py
+++ sheet/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Sheet
 from .forms import Sheet_Form
+
 from django.contrib.auth import get_user_model
 
 @login_required
 def index(request):
-    User = get_user_model()
-    foo = User
-    form = Sheet_Form()
-    return render(request, 'sheet/sheet.html')
+    if request.method == 'GET':
+        Sheet_user = Sheet.objects.get(username = request.user.id)
+        form = Sheet_Form(instance = Sheet_user)
+    return render(request, 'sheet/sheet.html', {'form':form})
