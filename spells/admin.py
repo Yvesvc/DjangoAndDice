2080d86ccfd960f50bb190de11d13a3f0195e309
yvesvc <yvesss_710@hotmail.com>
2019-11-07 19:52:57 +0100

    spells:: create model spells_metadata

--- spells/admin.py
+++ spells/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from spells.models import Spells5E, My_Spells
+from spells.models import Spells5E, My_Spells, spells_metadata
 
 admin.site.register(Spells5E)
 admin.site.register(My_Spells)
+admin.site.register(spells_metadata)
